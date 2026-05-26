c4ebc8bd16eb1e439ff9accce90a0dc1b43b1cab
AlexE20 <00009423@uca.edu.sv>
2026-05-25 19:40:06 -0600

    feat:Adding GeneralResponse

--- src/main/java/com/uca/pncsegundoparcialveterinaria/dto/GeneralResponse.java
+++ src/main/java/com/uca/pncsegundoparcialveterinaria/dto/GeneralResponse.java
@@ -1,4 +1,13 @@
 package com.uca.pncsegundoparcialveterinaria.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
 public class GeneralResponse {
-}
+    Object data;
+    String message;
+}   
